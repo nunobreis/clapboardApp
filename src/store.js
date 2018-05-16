ceb0d943d3e01e3bfdc8a73b171a0458c1f12322
nunobreis <nuno@catalpa.io>
2018-05-17 01:08:10 +0200

    app first commit and redux basic configuration

---
+++ src/store.js
@@ -0,0 +1,29 @@
+import {
+  createStore,
+  combineReducers,
+  compose,
+  applyMiddleware
+} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+
+import movieBrowserReducer from './modules/movie-browser/movie-browser.reducer.js'
+
+const rootReducer = combineReducers({
+  movieBrowser: movieBrowserReducer
+});
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+  rootReducer,
+  undefined,
+  compose(
+    applyMiddleware(
+      thunkMiddleware,
+      loggerMiddleware
+    )
+  )
+);
+
+export default store;
