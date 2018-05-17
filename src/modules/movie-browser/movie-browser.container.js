fdcd66370b244ffcaec4a34855cfe93b41b1bed7
nunobreis <nuno@catalpa.io>
2018-05-18 01:01:00 +0200

    adds infinite scroll

--- src/modules/movie-browser/movie-browser.container.js
+++ src/modules/movie-browser/movie-browser.container.js
@@ -5,12 +5,40 @@ import { AppBar } from 'material-ui';
 
 import * as movieActions from './movie-browser.actions';
 import * as movieHelpers from './movie-browser.helpers';
+import * as scrollHelpers from '../common/scroll.helpers';
 import MovieList from './movie-list/movie-list.component';
 
 class MovieBrowser extends React.Component {
-  componentDidMount() {
-    this.props.getTopMovies(1);
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 1
+    };
+
+    this.handleScroll = this.handleScroll.bind(this);
   }
+
+  componentDidMount() {
+    window.onscroll = this.handleScroll;
+    this.props.getTopMovies(this.state.currentPage);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    const { topMovies } = this.props;
+    if (!topMovies.isLoading) {
+      let percentageScrolled = scrollHelpers.getScrollDownPercentage(window);
+      if (percentageScrolled > .8) {
+        const nextPage = this.state.currentPage + 1;
+        this.props.getTopMovies(nextPage);
+        this.setState({ currentPage: nextPage });
+      }
+    }
+  }
+
   render() {
     const { topMovies } = this.props;
     const movies = movieHelpers.getMoviesList(topMovies.response);
